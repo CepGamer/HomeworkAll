ed86d1a5d00fb25b02860a42fc52f522adfb4c73
CepGamer <bolotov.s.s@yandex.ru>
2013-09-17 00:50:01 +0400

    Almost

--- haskell/secondHW.hs
+++ haskell/secondHW.hs
@@ -16,3 +16,46 @@ myreverse as = myreverse' as []
 
 mytake 0 (x:xs) = x
 mytake a (x:xs) = mytake (a - 1) xs
+
+mymap _ [] = []
+mymap f (x:xs) = f x : mymap f xs
+
+myunpack [] = []
+myunpack (x:xs) = myconcat x $ myunpack xs
+
+mydec as bs = [(a, b) | a<-as, b<-bs]
+
+--(nominator, denom)
+
+myred (a, b) = (div a d, div b d) where d = gcd a b
+mymult (a, b) (c, d) = myred (a * c, b * d)
+mydiv (a, b) (c, d) = mymult (a, b) (d, c)
+mysum (a, b) (c, d) = myred (a * d + c * b, b * d)
+mysub (a, b) (c, d) = mysum (a, b) (-c, d)
+
+--(nominator, denom)
+
+myshift n as = myconcat as (replicate n (0, 1))
+mypad n as = myconcat (replicate n (0, 1)) as
+mynorm [] = []
+mynorm (a:as) = if fst a == 0 then mynorm as else (a:as)
+mydeg as = mylen (mynorm as) - 1
+
+myzip _ [] [] = []
+myzip op (a:as) (b:bs) = myconcat [op a b] (myzip op as bs)
+myzip' op as bs = myzip op (mypad (-d) as) (mypad d bs)
+  where d = (mylen as) - mylen bs
+
+--mydivpol :: [(Int, Int)] -> [(Int, Int)] -> ([(Int, Int)], [(Int, Int)])
+mydivpol as bs = mydiv' (mynorm as) (mynorm bs) []
+  where mydiv' a s q | ddif < 0 = (q, a)
+                     | otherwise = mydiv' a' s q'
+            where   ddif = (mydeg a) - (mydeg s)
+                    k = mydiv (head a) (head s)
+                    ks = mymap (mymult k) $ myshift ddif s
+                    q' = myzip' (+) q $ myshift ddif [k]
+                    a' = mynorm $ tail $ myzip' (-) a ks
+
+myprint' (a, b) = if b == 1 then show a else "(" ++ show a ++ "/" ++ show b ++ ")"
+myprint [] = ""
+myprint ((a, b):as) = (myprint' (a, b) ++ "x" ++ (if mydeg as == 0 then "" else "^(" ++ show (mydeg as + 1) ++ ")")) ++ "+" ++ myprint as
