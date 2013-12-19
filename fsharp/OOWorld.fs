c121e87ad6cfccc0d1d529a5033b772122e54aa7
CepGamer <blossom120@gmail.com>
2013-12-19 22:13:13 +0400

    Finished GUI

--- fsharp/OOWorld.fs
+++ fsharp/OOWorld.fs
@@ -5,30 +5,29 @@ type Terrain = Water | Earth | Mountains
 
 [<AbstractClass>]
 type Robot(name: string, start: PointF, batteryCharge: int32) = 
-    class
-        inherit obj()
-        let mutable pos = start
-        let mutable currCharge = batteryCharge
-        let mutable psTime = 0
-        member x.charge with get () = currCharge and set (value) = currCharge <- value
-        member x.name = name
-        member x.position with get () = pos and set(value) = pos <- value
-        member x.toString() = "No string fo' robots!"
-        member x.pauseTime with get () = psTime and set (value) = psTime <- value
-        abstract member move: PointF -> string
-        abstract member recharge: int32 -> string
-    end
+    let mutable pos = start
+    let mutable currCharge = batteryCharge
+    let mutable psTime = 0
+    let mutable isTurr = false
+    member x.charge with get () = currCharge and set (value) = currCharge <- value
+    member x.name = name
+    member x.position with get () = pos and set(value) = pos <- value
+    member x.toString() = "No string fo' robots!"
+    member x.pauseTime with get () = psTime and set (value) = psTime <- value
+    member x.isTurret with get () = isTurr and set (value: bool) = isTurr <- value
+    abstract member move: PointF -> string
+    abstract member recharge: int32 -> string
 
-type Car(name, start, charge) = 
+type Car(name, start, charge) as this = 
     inherit Robot(name, start, charge)
         override x.move direction = "We are going to go in 4 directions &, if there is another Robot, fight for our lives!"
-        override x.recharge time = "In here we are rechargin our battery"
+        override x.recharge time = "In here we are recharging our battery"
 
 type Omni(name, start, charge, turPower: int32) = 
     inherit Car(name, start, charge)
         override x.move direction = "And here I am going to move in 8 directions!"
         override x.recharge time = "Same thing - we are recharching"
-        member x.turret = turPower  //  Damn dis n***a has a turret!
+        member x.turret = turPower
 
 type Boat(name, start, charge) = 
     inherit Robot(name, start, charge)
@@ -44,4 +43,3 @@ type Heli(name, start, charge) =
     inherit Jet(name, start, charge)
         override x.move direction = "Move in 8 direction on any surface"
         override x.recharge time = "At any time in any place!"
-
--- fsharp/ui.fs
+++ fsharp/ui.fs
@@ -1,92 +1,107 @@
 ﻿module ui
 open System.Windows.Forms
 open System.Drawing
+open System.IO
+open System.Text
+open System
 
 type World = Water | Terrain | Mountain
 
+type MButton() as this = 
+    member x.create name =
+        let mutable lc = 
+            match name with
+            | "Water" -> 1
+            | "Terrain" -> 2
+            | "Mountain" -> 3
+            | "Import" -> 4
+            | _ -> failwith "Wrong button name"
+        new Button( Text = name,
+                    Height = 70,
+                    Width = 70,
+                    Visible = true,
+                    Enabled = true,
+                    Left = 520,
+                    Top = 10 * lc + 70 * (lc - 1))
+
 type MForm() as this = 
-    class
-        inherit Form(Width = 610, Height = 545, MaximizeBox = false, FormBorderStyle = FormBorderStyle.Fixed3D)
-        let horBord = 500 / 50
-        let verBord = 500 / 50
-        let mutable penType = Water
-        let mutable map = Array2D.init 50 50 (fun _ _ -> Water)
-        let buttonW = new Button(   Text = "Water",
-                                    Height = 70,
-                                    Width = 70,
-                                    Visible = true,
-                                    Enabled = true,
-                                    Left = 520,
-                                    Top = 10)
-        
-        let buttonT = new Button(   Text = "Terrain",
-                                    Height = 70,
-                                    Width = 70,
-                                    Visible = true,
-                                    Enabled = true,
-                                    Left = 520,
-                                    Top = 10 * 2 + 70)
-        
-        let buttonM = new Button(   Text = "Mountain",
-                                    Height = 70,
-                                    Width = 70,
-                                    Visible = true,
-                                    Enabled = true,
-                                    Left = 520,
-                                    Top = 10 * 3 + 70 * 2)
-        
-        let buttonS = new Button(   Text = "Import",
-                                    Height = 70,
-                                    Width = 70,
-                                    Visible = true,
-                                    Enabled = true,
-                                    Left = 520,
-                                    Top = 10 * 4 + 70 * 3)
-        
-        let easel = new PictureBox( Height = 500,
-                                    Width = 500,
-                                    Top = 10,
-                                    Left = 10,
-                                    Visible = true,
-                                    Enabled = true)
+    inherit Form(Width = 610, Height = 550, MaximizeBox = false, FormBorderStyle = FormBorderStyle.Fixed3D)
+    let blue = new SolidBrush(Color.Blue)
+    let green = new SolidBrush(Color.Green)
+    let brown = new SolidBrush(Color.Brown)
+    let creator = new MButton()
 
-        let easPaint (e: PaintEventArgs) = 
-            e.Graphics.Clear(Color.White)
-            for i in 0..49 do
-                for j in 0..49 do
-                    let br = 
-                        match map.[i, j] with
-                        | Water -> new SolidBrush(Color.Blue)
-                        | Terrain -> new SolidBrush(Color.Green)
-                        | Mountain -> new SolidBrush(Color.Brown)
-                    e.Graphics.FillRectangle (br, horBord * i, verBord * j, horBord, verBord)
+    let filePath = sprintf "%s\\import.txt" <| Environment.GetFolderPath Environment.SpecialFolder.MyDocuments
+    let sw = new StreamWriter (filePath)
+    
+    let horBord = 500 / 50
+    let verBord = 500 / 50
+    let mutable penType = Water
+    let mutable map = Array2D.init 50 50 (fun _ _ -> Water)
+        
+    let buttonW = creator.create "Water"
+    let buttonT = creator.create "Terrain"
+    let buttonM = creator.create "Mountain"
+    let buttonS = creator.create "Import"
 
-        let easMD (e: MouseEventArgs) = 
-            let a = e.Location
-            map.[a.X / 10, a.Y / 10] <- penType
-            easel.Refresh ()
 
-        let easMM (e: MouseEventArgs) = 
-            let a = e.Location
-            if e.Button <> MouseButtons.None then 
-                if a.X < 500 && a.X >= 0 && a.Y < 500 && a.Y >= 0 then
-                    if map.[a.X / 10, a.Y / 10] <> penType then 
-                        map.[a.X / 10, a.Y / 10] <- penType
-                        easel.Refresh()
+    let easel = new PictureBox( Height = 500,
+                                Width = 500,
+                                Top = 10,
+                                Left = 10,
+                                Visible = true,
+                                Enabled = true)
 
-        do
-            this.Controls.Add buttonW
-            this.Controls.Add buttonT
-            this.Controls.Add buttonM
-            this.Controls.Add buttonS
-            this.Controls.Add easel
-            buttonW.Click.Add (fun _ -> penType <- Water)
-            buttonT.Click.Add (fun _ -> penType <- Terrain)
-            buttonM.Click.Add (fun _ -> penType <- Mountain)
-            easel.Paint.Add easPaint
-            easel.MouseDown.Add easMD
-            easel.MouseMove.Add easMM
-    end
+    let easPaint (e: PaintEventArgs) = 
+        e.Graphics.Clear(Color.White)
+        for i in 0..49 do
+            for j in 0..49 do
+                let br = 
+                    match map.[i, j] with
+                    | Water -> blue
+                    | Terrain -> green
+                    | Mountain -> brown
+                e.Graphics.FillRectangle (br, horBord * i, verBord * j, horBord, verBord)
+
+    let easMD (e: MouseEventArgs) = 
+        let a = e.Location
+        map.[a.X / 10, a.Y / 10] <- penType
+        easel.Refresh ()
+
+    let easMM (e: MouseEventArgs) = 
+        let a = e.Location
+        if e.Button <> MouseButtons.None then 
+            if a.X < 500 && a.X >= 0 && a.Y < 500 && a.Y >= 0 then
+                if map.[a.X / 10, a.Y / 10] <> penType then 
+                    map.[a.X / 10, a.Y / 10] <- penType
+                    easel.Refresh()
+    
+    let import(arg: EventArgs) =         
+        let a = Array2D.map ( fun x -> match x with
+                                       | Water -> 'w'
+                                       | Mountain -> 'm'
+                                       | Terrain -> 't') map
+        let mutable x = ""
+        for i in 0..49 do
+            for j in 0..49 do
+                x <- sprintf "%s%s" x (a.[j, i].ToString())
+            x <- sprintf "%s\n" x
+        sw.Write x
+        sw.Flush()
+
+    do
+        this.Controls.Add buttonW
+        this.Controls.Add buttonT
+        this.Controls.Add buttonM
+        this.Controls.Add buttonS
+        this.Controls.Add easel
+        buttonW.Click.Add (fun _ -> penType <- Water)
+        buttonT.Click.Add (fun _ -> penType <- Terrain)
+        buttonM.Click.Add (fun _ -> penType <- Mountain)
+        buttonS.Click.Add import
+        easel.Paint.Add easPaint
+        easel.MouseDown.Add easMD
+        easel.MouseMove.Add easMM
 
 [<EntryPoint>]
 Application.Run(new MForm())
