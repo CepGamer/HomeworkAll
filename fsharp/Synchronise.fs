4702167d355696326676d301c9cf870378982034
CepGamer <blossom120@gmail.com>
2013-12-25 23:01:54 +0400

    Added Synchronise.fs with last hometask. Finished with GUI & OOWorld.

---
+++ fsharp/Synchronise.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+let curr = ref 0
+let quan = 5
+let rand = new Random()
+let arrOfRand = [|for i in 1..quan -> rand.Next(10000)|]
+
+let printProc num = 
+    lock curr (fun () ->
+        if !curr = num - 1 then
+            printfn "Process # %A finished its business" <| num
+            curr := !curr + 1
+            false
+        else true
+        )
+
+let proces num = 
+    async {
+        do! Async.Sleep arrOfRand.[num - 1]
+        while (printProc num) do
+            do! Async.Sleep 100
+    }
+
+[<EntryPoint>]
+let main argc = 
+    let arr = [|for i in 1..quan -> proces i|]
+    arr |> Async.Parallel |> Async.RunSynchronously |> ignore
+    0
