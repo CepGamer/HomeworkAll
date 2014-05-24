89afc646ec7ca2cbb1e2f70371143d250877fa9e
Bolotov Sergey <bolotov.s.s@ya.ru>
2014-05-24 15:32:56 +0400

    Added localization app

---
+++ C#/LocalizationApp/LocalizationApp/ElfsBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocalizationApp
+{
+    public partial class ElfsBuilderForm : Form
+    {
+        public ElfsBuilderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ChangeLanguageButton_Click(object sender, EventArgs e)
+        {
+            this.ChangeLanguage();
+        }
+
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
