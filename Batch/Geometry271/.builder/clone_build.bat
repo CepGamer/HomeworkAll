fe270cf043e35de3daa177109b01742044536dde
Bolotov Sergey <bolotov.s.s@yandex.ru>
2015-03-27 18:35:00 +0300

    Renamed Batch

---
+++ Batch/Geometry271/.builder/clone_build.bat
@@ -0,0 +1,21 @@
+@echo off
+
+if not %my_builder_start%==1 goto :EOF
+
+git clone https://github.com/CepGamer/%repo_name%.git>nul 2>%err_file%
+if errorlevel 1 goto :fail_git
+MSBuild.exe %repo_name%/%proj_name%.sln>%err_file%
+if not %ERRORLEVEL%==0  goto :fail_build
+
+set email_message="Build successfull"
+goto :EOF
+
+:fail_git
+set git_fail=1
+set fail=1
+goto :EOF
+
+:fail_build
+set build_fail=1
+set fail=1
+goto :EOF
---
+++ Batch/Geometry271/.builder/settings.bat
@@ -0,0 +1,26 @@
+@echo off
+
+if not %my_builder_start%==1 goto :EOF
+
+set repo_name=Geometry271
+set proj_name=MainApp
+
+set msbuildpath="C:\Program Files (x86)\MSBuild\12.0\Bin"
+set blatpath="C:\Users\Сергей\Downloads\blat311\full"
+
+set fail=0
+set git_fail=0
+set build_fail=0
+set err_file="error.log"
+
+set email_message=""
+set email_sender=
+set email_subject=""
+set email_list=mail_list.txt
+set email_log=""
+
+set bin_list=bin.txt
+
+set mail_addr="lol15.ivanov@mail.ru"
+set mail_pass="115lalala"
+set mail_serv=smtp.mail.ru
