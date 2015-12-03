8a8a09f52ea9e3788d654d273b96c8e8ca19f68c
Bolotov Sergey <bolotov.s.s@yandex.ru>
2015-12-03 16:23:14 +0300

    Added parsing from words

--- python/Language Processing/script.py
+++ python/Language Processing/script.py
@@ -2,8 +2,19 @@
 
 def parseData(filename):
 	from nltk.stem.snowball import RussianStemmer
-	# f = open(filename, 'r')
+	from re import sub
+	f = open(filename, 'r').read().split('\n')
+	posts = [0] * len(f)
+	for i, string in enumerate(f):
+		try:
+			smile = sub('(:-*\)+)|(\)\)+)', 'smile', string.split('\t')[3])
+			posts[i] = sub('(:+-*\(+)|(\(\(+)', 'sad', smile)
+		except Exception:
+			posts[i] = ''
+	
+	
 	stemmer = RussianStemmer()
+	
 
 def train():
 	from pybrain.tools.shortcuts import buildNetwork
