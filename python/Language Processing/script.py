6b05482b953ea85851107afcb3dbdafa69d91c94
Bolotov Sergey <bolotov.s.s@yandex.ru>
2015-12-04 07:11:06 +0300

    Added word2vec model

--- python/Language Processing/script.py
+++ python/Language Processing/script.py
@@ -1,13 +1,16 @@
 # coding=UTF-8
 
+happy = u'happy'
+sad = u'sad'
+
 def parseData(filename):
 	from re import sub
 	f = open(filename, 'r').read().split('\n')
 	posts = [0] * len(f)
 	for i, string in enumerate(f):
 		try:
-			smile = sub('(:-*\)+)|(\)\)+)', 'smile.', string.split('\t')[3])
-			posts[i] = sub('(:+-*\(+)|(\(\(+)', 'sad.', smile)
+			smile = sub(u'(:-*\)+)|(\)\)+)', happy + u'.', string.split('\t')[3])
+			posts[i] = sub(u'(:+-*\(+)|(\(\(+)', sad + u'.', smile)
 		except Exception:
 			posts[i] = ''
 	return posts
@@ -16,26 +19,52 @@ def stemData(posts):
 	from nltk.stem.snowball import RussianStemmer
 	from nltk import word_tokenize, sent_tokenize
 	stemmer = RussianStemmer()
+	# renew smiles
+	happy = stemmer.stem(happy)
+	sad = stemmer.stem(sad)
+	
+	toRet = []
 	for i in range(0, len(posts)):
-		sentences = sent_tokenize(posts[i])
+		sentences = sent_tokenize(posts[i].decode('utf8'))
 		for j in range(0, len(sentences)):
 			words = word_tokenize(sentences[j])
 			for k in range(0, len(words)):
-				words[k] = stemmer.stem(words[k])
-			sentences[j] = u' '.join(words)
-		posts[i] = u''.join(sentences)
-	return posts
+				try:
+					words[k] = stemmer.stem(words[k])
+				except Exception:
+					print words[k]
+			try:
+				sentences[j] = u' '.join(words)
+			except Exception:
+				print words
+				sentences[j] = ''
+		toRet += sentences
+	return toRet
 
-def train():
-	from pybrain.tools.shortcuts import buildNetwork
-	net = buildNetwork(1, 1, 100)
-
+def train(sentences):
+	from gensim.models import Word2Vec
+	model = Word2Vec(sentences, size=100)
+	return model
+	
+def getResults(model):
+	t = u'Positive smile alike:\n'
+	for x in model.most_similar(positive = [happy], negative = [sad]):
+		t += x
+		t += u'\n'
+	t += 'Negative smile alike:\n'
+	for x in model.most_similar(positive = [sad], negative = [happy]):
+		t += x
+		t += u'\n'
+	print t
+	return t
+	
 filename = 'train_content.csv'
 
 def do():
-	# x = parseData(filename)
-	x = [u'Привет! Как дела? А я вот по улице гуляю... Ага, так и есть :)']
+	x = parseData(filename)
 	y = stemData(x)
-	print y[0]
-
+	z = train(y)
+	t = getResults(z)
+	open('tmp.txt', 'w').write(t)
+	
 do()
