a835ff0ef2b2f772a22729389ed4a350d674e902
Bolotov Sergey <bolotov.s.s@yandex.ru>
2016-01-25 00:08:44 +0300

    Added DavBoul

--- python/StochasticProgramming/task3.py
+++ python/StochasticProgramming/task3.py
@@ -3,23 +3,18 @@ __author__ = 'Sergei Bolotov'
 import task2 as km
 import numpy as np
 
-def bgss(w, clusters, centers):
+def bgss(w, clusters, centers, ns, degree = 2):
     g = km.barycenter(w)
-    ns = [0] * len(centers)
     res = 0
-    for i in clusters:
-        ns[i] += 1
     for i, c in enumerate(centers):
-        x = np.euclidean(g, c)
-        x *= 2
+        x = pow(np.euclidean(g, c), degree)
         res += ns[i] * x
     return res
 
-def wgss(w, clusters, center, clustNum):
+def wgss(w, clusters, center, clustNum, degree = 2):
     res = 0
     for i, x in enumerate(w):
-        t = np.euclidean(x, center)
-        t *= 2
+        t = pow(np.euclidean(x, center), degree)
         res += t
     return res
 
@@ -39,14 +34,36 @@ if __name__ = "main":
         x, clusters, y = km.kmeans(w, k)
         centers = km.minimisation(w, clusters, k)
         
-        # DavBoul count
-        
+        # prelim counts
+        ns = [0] * k
+        for i in clusters:
+            ns[i] += 1
+        tb = False
+        for i in ns:
+            if i == 0:
+                tb = True
+                break
+        if tb:
+            continue
+
         # CalHar count
         wgssSum = 0
-        bgssSum = bgss(w, clusters, centers)
+        bgssSum = bgss(w, clusters, centers, ns)
         for i in range(k):
             wgssSum += wgss(w, clusters, centers[i], i)
         CalHar[k - minK] = bgssSum * (len(clusters) - k) / (wgssSum * (k - 1))
+        
+        # DavBoul count
+        meanDists = [0] * k
+        for i in range(k):
+            meanDists[i] = wgss(w, clusters, centers[i], i, 1) / ns[i]
+        for i in range(k):
+            t = 0
+            for j in range(k):
+                if i != j:
+                    t = max(t, (meanDists[i] + meanDists[j]) / np.euclidean(centers[i], centers[j]))
+            DavBoul[k - minK] += t
+        DavBoul[k - minK] /= k
         
         # Rand count
         
